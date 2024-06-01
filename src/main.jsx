909bcd6c2aaf74b5723c6bd22d7da368d32b0c9a
sadiqur057 <sadiqur057@gmail.com>
2024-06-01 17:43:41 +0600

    configured tanstack queries

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { Routes } from './routes/Routes.jsx';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={Routes} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={Routes} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
