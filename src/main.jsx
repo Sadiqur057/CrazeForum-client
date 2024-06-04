87f2bc3bc16b1c6976fddd100b78895e17078c5e
sadiqur057 <sadiqur057@gmail.com>
2024-06-04 09:31:31 +0600

    added queryclientprovider for using tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,24 @@ import './index.css'
 import {
   RouterProvider,
 } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { Routes } from './routes/Routes.jsx';
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AuthProvider from './proviers/AuthProvider';
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={Routes} />
+      <AuthProvider>
+        <HelmetProvider>
+          <RouterProvider router={Routes}></RouterProvider>
+        </HelmetProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
