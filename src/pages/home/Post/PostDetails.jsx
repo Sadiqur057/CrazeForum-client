9396964222070517ec7c06ab5b33b675f7854014
sadiqur057 <sadiqur057@gmail.com>
2024-06-04 09:27:46 +0600

    fetched comments from databaase in comments section

--- src/pages/home/Post/PostDetails.jsx
+++ src/pages/home/Post/PostDetails.jsx
@@ -7,7 +7,9 @@ import { useParams } from 'react-router-dom';
 import Container from '@/components/container/Container';
 import { FaRegComments } from 'react-icons/fa6';
 import { IoMdTime } from 'react-icons/io';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { AuthContext } from '@/proviers/AuthProvider';
+import useLoadComments from '@/hooks/useLoadComments';
 
 const PostDetails = () => {
 
@@ -15,6 +17,8 @@ const PostDetails = () => {
   const { id: postId } = useParams()
   const [upVote, setUpVote] = useState(false)
   const [downVote, setDownVote] = useState(false)
+  const { user } = useContext(AuthContext)
+  const [comments, isCommentsLoading, refetchComments] = useLoadComments()
 
   const { data: postDetails = {}, isLoading, refetch } = useQuery({
     queryKey: ['post', postId],
@@ -26,8 +30,8 @@ const PostDetails = () => {
 
   const { mutate: updateUpVote } = useMutation({
     mutationFn: async (data) => {
-      const res = await axiosCommon.post(`/post/upvote/${postId}`,data)
-      if(res.data){
+      const res = await axiosCommon.post(`/post/upvote/${postId}`, data)
+      if (res.data) {
         refetch()
       }
     }
@@ -35,8 +39,8 @@ const PostDetails = () => {
 
   const { mutate: updateDownVote } = useMutation({
     mutationFn: async (data) => {
-      const res = await axiosCommon.post(`/post/downvote/${postId}`,data)
-      if(res.data){
+      const res = await axiosCommon.post(`/post/downvote/${postId}`, data)
+      if (res.data) {
         refetch()
       }
     }
@@ -45,37 +49,63 @@ const PostDetails = () => {
   const handleUpVote = () => {
     if (downVote) {
       setDownVote(false)
-      updateDownVote({update:-1})
+      updateDownVote({ update: -1 })
     }
     setUpVote(true);
-    updateUpVote({update:1})
-    
+    updateUpVote({ update: 1 })
+
 
   }
   const handleDownVote = () => {
     if (upVote) {
       setUpVote(false)
-      updateUpVote({update:-1})
+      updateUpVote({ update: -1 })
     }
     setDownVote(true)
-    updateDownVote({update:1})
+    updateDownVote({ update: 1 })
+  }
+
+  const {mutate} = useMutation({
+    mutationFn: async(commentsDetails)=>{
+      const res = await axiosCommon.post('/comment',commentsDetails)
+      if(res.data.insertedId){
+        console.log("success")
+        refetchComments()
+      }
+    }
+  })
+
+  const handleComments = (e)=>{
+    e.preventDefault()
+    const comment = e.target.comment.value;
+    const post_id = postId
+    const commenter_name = user?.displayName;
+    const commenter_email = user?.email;
+    const feedback = "";
+    const isReported = false;
+    const commentsDetails = {
+      comment, commenter_name, commenter_email, post_id, feedback, isReported
+    }
+    mutate(commentsDetails)
+    e.target.comment.value = ''
     
 
   }
 
   console.log(postDetails)
-  if (isLoading) {
+  if (isLoading || isCommentsLoading) {
     return <p>Loading</p>
   }
 
-  const { _id, author_image, author_email, author_name, post_title, description, tag, up_vote_count, down_vote_count, comment_count, posted_time } = postDetails || {}
+  const { author_image, author_email, author_name, post_title, description, tag, up_vote_count, down_vote_count, comment_count, posted_time } = postDetails || {}
+  console.log(author_email)
   return (
     <Container>
       <div className="px-8 py-4">
         <div className='bg-[#f3f3f5] dark:bg-gray-800 p-5 rounded-xl'>
           <div className="flex items-center justify-between">
             <span className="text-sm font-light text-gray-600 dark:text-gray-400 flex gap-2 items-center"><IoMdTime className='text-base mt-[1px]'></IoMdTime> {posted_time}</span>
-            <p>#Comedy</p>
+            <p>#{tag}</p>
           </div>
 
           <div className="mt-2">
@@ -113,25 +143,28 @@ const PostDetails = () => {
           </div>
         </div>
 
-        <div className='md:grid grid-cols-12 gap-10 mt-6'>
-          <form className='col-span-6 order-2 mb-5 lg:mb-0'>
-            <textarea className='border  rounded-md border-c-secondary w-full outline-none p-2 bg:white dark:bg-gray-800' placeholder='write a comment'></textarea>
-            <input type="submit" className='bg-c-secondary w-full py-2 rounded-md text-white font-semibold' value="Comment" />
-          </form>
+        <div className='md:grid grid-cols-12 gap-4 mt-6 flex'>
+
+          <div className='col-span-6 order-2 mb-5 lg:mb-0 flex gap-3'>
+          <div className='w-10 h-10'>
+            <img className='w-full rounded-full' src={user?.photoURL} alt="" />
+          </div>
+            <form onSubmit={handleComments}>
+              <textarea className='border  rounded-md border-c-secondary w-full outline-none p-2 bg:white dark:bg-gray-800' name="comment" placeholder='write a comment'></textarea>
+              <input type="submit" className='bg-c-secondary w-full py-2 rounded-md text-white font-semibold' value="Comment" />
+            </form>
+          </div>
           <div className='col-span-6 order-1 space-y-4'>
             <div className="bg-[#f3f3f5] dark:bg-gray-800 p-5 rounded-xl">
-              <h2 className='text-2xl font-bold'>Comments</h2>
-              <div>
-              <hr className='my-4 bg-[#eeeeee]'/>
-                <p className="font-semibold text-gray-600 cursor-pointer dark:text-gray-200 mb-1" >{author_name}</p>
-                <p className="text-gray-600 cursor-pointer dark:text-gray-200 leading-[22px]" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ducimus obcaecati </p>
-              </div>
+              <h2 className='text-2xl font-bold'>Comments ({comments.length})</h2>
+              {
+                comments.map(comment=><div key={comment._id}>
+                  <hr className='my-4 bg-[#eeeeee]' />
+                  <p className="font-semibold text-gray-600 cursor-pointer dark:text-gray-200 mb-1" >{comment.commenter_name}</p>
+                  <p className="text-gray-600 cursor-pointer dark:text-gray-200 leading-[22px]" >{comment.comment} </p>
+                </div>)
+              }
 
-              <div>
-              <hr className='my-4 bg-[#eeeeee]'/>
-                <p className="font-semibold text-gray-600 cursor-pointer dark:text-gray-200 mb-1" >{author_name}</p>
-                <p className="text-gray-600 cursor-pointer dark:text-gray-200 leading-[22px]" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ducimus obcaecati </p>
-              </div>
             </div>
 
 
