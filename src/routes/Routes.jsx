f5bf9a1aaeeafdc9b2a878f04b8116eb6be9c437
sadiqur057 <sadiqur057@gmail.com>
2024-06-04 09:29:43 +0600

    added admin routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,7 +2,16 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import Root from "../layout/Root";
-import Home from "../pages/home/Home";
+import Home from "../pages/home/Home/Home"
+import PostDetails from "@/pages/home/Post/PostDetails";
+import Login from "@/pages/login/Login";
+import Register from "@/pages/register/Register";
+import Membership from "@/pages/Membership/Membership";
+import Dashboard from "@/pages/Dashboard/Dashboard";
+import UserProfile from "@/pages/Dashboard/UserProfile/UserProfile";
+import UserPost from "@/pages/Dashboard/UserProfile/UserPost";
+import AddPost from "@/pages/Dashboard/UserProfile/AddPost";
+import AdminProfile from "@/pages/Dashboard/AdminProfile/AdminProfile";
 
 export const Routes = createBrowserRouter([
   {
@@ -12,7 +21,45 @@ export const Routes = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>
-      }
+      },
+      {
+        path: '/post/:id',
+        element: <PostDetails></PostDetails>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/membership',
+        element: <Membership></Membership>
+      },
+    ]
+  },
+  {
+    path: 'dashboard',
+    element: <Dashboard></Dashboard>,
+    children:[
+      {
+        path:'userProfile',
+        element:<UserProfile></UserProfile>
+      },
+      {
+        path:'userPost',
+        element:<UserPost></UserPost>
+      },
+      {
+        path:'addPost',
+        element:<AddPost></AddPost>
+      },
+      {
+        path:'adminProfile',
+        element:<AdminProfile></AdminProfile>
+      },
     ]
   }
 ])
