d38805466cbb0cc1f44f5e8f2ff2d01aa8b963e6
sadiqur057 <sadiqur057@gmail.com>
2024-06-05 23:11:49 +0600

    removed same data

--- src/pages/home/Banner/TagBox.jsx
+++ src/pages/home/Banner/TagBox.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 const TagBox = ({ Icon, title }) => {
   return (
-    <div className='backdrop-blur-sm py-4 lg:py-6 px-6 shadow-lg border-2 border-t-c-secondary border-r-c-secondary border-b-c-primary border-l-c-primary flex flex-col items-center space-y-3 rounded-md cursor-pointer'>
+    <div className='backdrop-blur-sm py-4 lg:py-6 px-6 shadow-lg border-2 border-t-c-secondary border-r-c-secondary border-b-c-primary border-l-c-primary flex flex-col items-center space-y-3 rounded-md'>
       <h3 className='font-semibold md:text-xl'>{title}</h3>
       <Icon className='text-2xl md:text-3xl'></Icon>
     </div>
