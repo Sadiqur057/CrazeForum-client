a8fd0dc7e10a244a593e54635cb530eaa77b259e
sadiqur057 <sadiqur057@gmail.com>
2024-06-05 23:08:43 +0600

    handled search and sort functionality

--- src/pages/home/Home/Home.jsx
+++ src/pages/home/Home/Home.jsx
@@ -1,13 +1,33 @@
 import Banner from "../Banner/Banner";
 import Container from "../../../components/container/Container"
 import PostContainer from "../Post/PostContainer";
+import { useEffect, useState } from "react";
+import useSearchPostByTag from "@/hooks/useSearchPostByTag";
 
 const Home = () => {
 
+  const [keyword, setKeyword] = useState('all')
+  const [postByTag, postsByTagLoading, refetchPostsByTag] = useSearchPostByTag(keyword)
+
+  const [displayPosts, setDisplayPosts] = useState([])
+
+  useEffect(() => {
+    if (!postsByTagLoading) {
+      setDisplayPosts(postByTag);
+    }
+  }, [postsByTagLoading, postByTag])
+
   return (
     <Container>
-      <Banner></Banner>
-      <PostContainer></PostContainer>
+      <Banner
+        setKeyword={setKeyword}
+        refetchPostsByTag={refetchPostsByTag}></Banner>
+      <PostContainer
+        setKeyword={setKeyword}
+        setDisplayPosts={setDisplayPosts}
+        displayPosts={displayPosts}
+        postsByTagLoading={postsByTagLoading}
+        refetchPostsByTag={refetchPostsByTag}></PostContainer>
     </Container>
   );
 };
