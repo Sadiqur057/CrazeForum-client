490284677cd795e1fc792e217db57213a0ffa293
sadiqur057 <sadiqur057@gmail.com>
2024-06-05 23:03:19 +0600

    configured react toasify

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../pages/shared/NavBar";
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const Root = () => {
   return (
     <>
       <NavBar></NavBar>
+      <ToastContainer />
       <main className="dark:bg-gray-900 min-h-[calc(100vh-82px)]">
           <Outlet></Outlet>
       </main>
