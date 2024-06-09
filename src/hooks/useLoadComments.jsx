5c4d9c290a3ad3a07929b34c2ef3ce6d4e3931f8
sadiqur057 <sadiqur057@gmail.com>
2024-06-09 10:36:54 +0600

    added loading state to show custom skeleton while loading

--- src/hooks/useLoadComments.jsx
+++ src/hooks/useLoadComments.jsx
@@ -2,16 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosCommon from "./useAxiosCommon";
 
 
-const useLoadComments = () => {
+const useLoadComments = (id, currentPage) => {
   const axiosCommon = useAxiosCommon()
-  const { data: comments = [], isLoading: isCommentsLoading, refetch:refetchComments } = useQuery({
-    queryKey: ['comments'],
+  const { data = {result : [], commentCounts:0}, isLoading: isCommentsLoading, refetch:refetchComments } = useQuery({
+    queryKey: ['comments',id, currentPage],
     queryFn: async () => {
-      const res = await axiosCommon.get('/comments')
+      const res = await axiosCommon.get(`/comments/${id}?page=${currentPage}`)
       return res.data
     }
   })
-  return [comments, isCommentsLoading, refetchComments]
+  const comments = data.result;
+  const commentCounts = data.commentCounts;
+  return [comments, isCommentsLoading, refetchComments, commentCounts]
 };
 
 export default useLoadComments;
